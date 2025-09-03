934813eabc17ddac763208171380c96f459746df
Aurélio Venturelli <venturelli.dev@gmail.com>
2025-09-02 21:25:54 -0300

    feat: Adição do componente/ui Button.tsx

---
+++ frontend/app/src/components/ui/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "flowbite-react";
+
+const newButton = () => {
+	return <Button>Button</Button>;
+};
+
+export default newButton;
---
+++ frontend/app/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import newButton from "@/components/ui/Button";
+export default function Home() {
+	return <div>{newButton()}</div>;
+}
